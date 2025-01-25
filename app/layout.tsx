77fda93776a17a4d03b21453c58feb4e54348e04
Henry <franzhenry46@gmail.com>
2025-01-25 19:05:52 -0300

    chore: configurando dependencias e paleta de cores e estilos de fontes

---
+++ app/layout.tsx
@@ -0,0 +1,25 @@
+import type { Metadata } from "next";
+import { Open_Sans } from "next/font/google";
+import "./globals.css";
+
+const openSans = Open_Sans({
+  weight: ["700", "400"],
+});
+
+export const metadata: Metadata = {
+  title: "Xp zone",
+  description:
+    "Nunca foi tão fácil conseguir aquele jogo ou console tão sonhado com a XP Zone.",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="pt-br">
+      <body className={`${openSans.style} antialiased`}>{children}</body>
+    </html>
+  );
+}
