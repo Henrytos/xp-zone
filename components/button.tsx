55f7ddfcc3010090d65e90dc4bf48651ab2d130c
Henry <franzhenry46@gmail.com>
2025-01-30 23:31:10 -0300

    novas variações de botão

--- components/button.tsx
+++ components/button.tsx
@@ -1,11 +1,13 @@
+import { VariantProps } from "class-variance-authority";
 import { ComponentProps, ReactNode } from "react";
 import { tv } from "tailwind-variants";
 const variantsButton = tv({
-  base: "font-bold text-2xl font-open-sans px-5 py-3 rounded cursor-pointer",
+  base: "font-bold text-2xl font-open-sans px-5 py-3 rounded-md cursor-pointer flex inline items-center",
   variants: {
     color: {
       primary: "bg-radient-horizontal text-white",
       secundary: "border text-white",
+      terceary: "bg-primar text-white",
     },
   },
   defaultVariants: {
@@ -13,10 +15,14 @@ const variantsButton = tv({
   },
 });
 
-interface RootProps extends ComponentProps<"button"> {
-  color?: "primary" | "secundary";
-}
-function Root({ children, color, className, ...props }: RootProps) {
+type VariantsButton = VariantProps<typeof variantsButton>;
+
+function Root({
+  children,
+  color,
+  className,
+  ...props
+}: ComponentProps<"button"> & VariantsButton) {
   return (
     <button
       className={variantsButton({
